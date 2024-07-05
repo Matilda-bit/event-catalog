78174f22ae1979e863a0011c90c4bfa7556b58d1
Matilda-bit <polinaovras@gmail.com>
2024-07-06 01:20:28 +0300

    error message

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom';
 //mark the place child element must be render to
 
 import MainNavigation from '../components/MainNavigation';
-import classes from './Root.module.css';
+
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
